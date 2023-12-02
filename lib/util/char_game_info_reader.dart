fd711dd6609c233f9d44975e6a2b02488eb6ee7e
Rigoberto Perez <rigperez@proton.me>
2023-12-02 13:37:49 -0500

    feat: Parse char game info (#8)
    * test: 💍 add tests for version reading
    
    * feat: 🎸 add read to read version from the file
    
    * refactor: 💡 print tostring of save file instead of path
    
    * test: 💍 make test to read file and assert fields that matter
    
    * fix: 🐛 add remaining fields and a toString method
    
    * fix: 🐛 read the remaining properties of game info
    
    * test: 💍 add test data file I forgot

--- lib/util/char_game_info_reader.dart
+++ lib/util/char_game_info_reader.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:typed_data';
 
 import 'package:forgotten_key/models/char_game_info.dart';
 import 'package:loggy/loggy.dart';
@@ -11,13 +12,54 @@ class CharGameInfoReader with UiLoggy {
     RandomAccessFile? file;
 
     try {
+      // Total content of GameInfo in save file should be 179 bytes
       file = await File(path).open();
       final header = _readHeader(file);
+      final gameVersion = _readVersion(file);
+      // Read the current save game time. Every 300 game time units is 1 hour. The actual displayed game time is 2100 units more than is displayed.
+      final gameTime = _readUInt32(file);
+      final unknown0 = file.readSync(12);
+      final partyGold = _readUInt32(file);
+      final unknown1 = file.readSync(4);
+      final inPartyCharOffset = _readUInt32(file);
+      final inPartyCharCount = _readUInt32(file);
+      final unknown2 = file.readSync(8);
+      final outPartyCharOffset = _readUInt32(file);
+      final outPartyCharCount = _readUInt32(file);
+      final globalVarOffset = _readUInt32(file);
+      final globalVarCount = _readUInt32(file);
+      final areaRes = file.readSync(8);
+      final unknown3 = file.readSync(4);
+      final journalCount = _readUInt32(file);
+      final journalOffset = _readUInt32(file);
+      final partyReputation = _readUInt8(file);
+      final unknown4 = file.readSync(19);
+      final afterJournalOffset = _readUInt32(file);
+      final unknown5 = file.readSync(72);
 
       return CharGameInfo(
-        name: 'Unknown',
         path: path,
         header: header,
+        gameVersion: gameVersion,
+        gameTime: gameTime,
+        unknown0: unknown0,
+        partyGold: partyGold,
+        unknown1: unknown1,
+        inPartyCharOffset: inPartyCharOffset,
+        inPartyCharCount: inPartyCharCount,
+        unknown2: unknown2,
+        outPartyCharOffset: outPartyCharOffset,
+        outPartyCharCount: outPartyCharCount,
+        globalVarOffset: globalVarOffset,
+        globalVarCount: globalVarCount,
+        areaRes: areaRes,
+        unknown3: unknown3,
+        journalCount: journalCount,
+        journalOffset: journalOffset,
+        partyReputation: partyReputation,
+        unknown4: unknown4,
+        afterJournalOffset: afterJournalOffset,
+        unknown5: unknown5,
       );
     } catch (e) {
       loggy.error(e);
@@ -32,12 +74,38 @@ class CharGameInfoReader with UiLoggy {
   String _readHeader(RandomAccessFile file) {
     final header = file.readSync(4);
     String headerString = String.fromCharCodes(header);
+    logDebug("header: $headerString");
 
     if (headerString != 'GAME') {
       logError('Invalid header');
       throw Exception('Invalid header of $headerString. It should be GAME.');
     }
-
     return headerString;
+  }
+
+  String _readVersion(RandomAccessFile file) {
+    final version = file.readSync(4);
+    String versionString = String.fromCharCodes(version);
+    logDebug("gameVersion: $versionString");
+
+    if (versionString != 'V2.0' && versionString != 'V2.1') {
+      logError('Invalid version: $versionString');
+      throw Exception(
+          "Invalid version of '$versionString'. Only 'V2.0' and 'V2.1' are supported. Version '$versionString' was found.");
+    }
+
+    return versionString;
+  }
+
+  int _readUInt32(RandomAccessFile file) {
+    final offset = file.readSync(4);
+    final byteData = ByteData.sublistView(offset);
+    final value = byteData.getUint32(0, Endian.little);
+    return value;
+  }
+
+  int _readUInt8(RandomAccessFile file) {
+    final value = file.readByteSync();
+    return value;
   }
 }
--- test/util/char_game_info_reader_test.dart
+++ test/util/char_game_info_reader_test.dart
@@ -9,18 +9,23 @@ void main() {
     }
   });
 
-  test('readHeader should return the correct header', () async {
-    final file = await File('test_file.gam').create();
-    final randomAccessFile = await file.open(mode: FileMode.write);
-    const header = 'GAME';
-    await randomAccessFile.writeString(header);
-    await randomAccessFile.flush();
-    await randomAccessFile.close();
-
-    final reader = CharGameInfoReader('test_file.gam');
-    final result = await reader.read();
-
-    expect(result.header, header);
+  test('should read valid save game file', () async {
+    final gameInfo = await CharGameInfoReader('test/data/BALDUR.gam').read();
+    expect(gameInfo, isNotNull);
+    expect(gameInfo.header, 'GAME');
+    expect(gameInfo.gameVersion, 'V2.0');
+    expect(gameInfo.gameTime, 122818);
+    expect(gameInfo.partyGold, 1959);
+    expect(gameInfo.inPartyCharOffset, 180);
+    expect(gameInfo.inPartyCharCount, 6);
+    expect(gameInfo.outPartyCharOffset, 31056);
+    expect(gameInfo.outPartyCharCount, 34);
+    expect(gameInfo.globalVarOffset, 116596);
+    expect(gameInfo.globalVarCount, 161);
+    expect(gameInfo.journalCount, 52);
+    expect(gameInfo.journalOffset, 130120);
+    expect(gameInfo.partyReputation, 140);
+    expect(gameInfo.afterJournalOffset, 130744);
   });
 
   test('should throw an exception if the header is not GAME', () async {
@@ -39,5 +44,24 @@ void main() {
             e is Exception &&
             e.toString() ==
                 'Exception: Invalid header of WAAA. It should be GAME.')));
+  });
+
+  test('should throw an exception if the version is not V2.0 or V2.1',
+      () async {
+    final file = await File('test_file.gam').create();
+    final randomAccessFile = await file.open(mode: FileMode.write);
+    const version = 'V2.2';
+    await randomAccessFile.writeString('GAME');
+    await randomAccessFile.writeString(version);
+    await randomAccessFile.flush();
+    await randomAccessFile.close();
+
+    final reader = CharGameInfoReader('test_file.gam');
+    expect(
+        () => reader.read(),
+        throwsA(predicate((e) =>
+            e is Exception &&
+            e.toString() ==
+                "Exception: Invalid version of 'V2.2'. Only 'V2.0' and 'V2.1' are supported. Version 'V2.2' was found.")));
   });
 }
