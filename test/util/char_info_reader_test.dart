e7cfdb87f173c5b103f202d2038a941a03c0e716
Rigoberto L. Perez <rigperez@proton.me>
2023-12-02 17:17:35 -0500

    test: 💍 improve test coverage and remove bad test

--- test/util/char_info_reader_test.dart
+++ test/util/char_info_reader_test.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:forgotten_key/util/char_game_info_reader.dart';
 import 'package:forgotten_key/util/char_info_reader.dart';
 
 void main() {
@@ -37,45 +36,27 @@ void main() {
     // Names are 21 characters long padded with 0x00. The first character is the null terminator.
     // Trell is the player created character and the rest are party NPCs. Non player
     // characters do not have their names displayed in the game on this variable.
-    expect(
-        charInfo.firstWhere((char) => char.name.contains('Trell')), isNotNull);
+    final trell = charInfo.firstWhere((char) => char.name.contains('Trell'));
+    expect(trell.toString(), contains('Trell'));
+    expect(trell, isNotNull);
     file.close();
   });
 
-  test('should throw an exception if the header is not GAME', () async {
+  test('should throw an exception if the file is incomplete', () async {
     final file = await File('test_file.gam').create();
     final randomAccessFile = await file.open(mode: FileMode.write);
-    const header = 'WAAA';
-    await randomAccessFile.writeString(header);
-    await randomAccessFile.flush();
-    await randomAccessFile.close();
-
-    final reader = CharGameInfoReader('test_file.gam');
-
-    expect(
-        () => reader.read(),
-        throwsA(predicate((e) =>
-            e is Exception &&
-            e.toString() ==
-                'Exception: Invalid header of WAAA. It should be GAME.')));
-  });
-
-  test('should throw an exception if the version is not V2.0 or V2.1',
-      () async {
-    final file = await File('test_file.gam').create();
-    final randomAccessFile = await file.open(mode: FileMode.write);
-    const version = 'V2.2';
+    const version = 'V2.1';
     await randomAccessFile.writeString('GAME');
     await randomAccessFile.writeString(version);
     await randomAccessFile.flush();
-    await randomAccessFile.close();
+    await randomAccessFile.setPosition(0);
 
-    final reader = CharGameInfoReader('test_file.gam');
+    final reader = CharInfoReader(file: randomAccessFile);
     expect(
-        () => reader.read(),
+        reader.read(1),
         throwsA(predicate((e) =>
-            e is Exception &&
+            e is RangeError &&
             e.toString() ==
-                "Exception: Invalid version of 'V2.2'. Only 'V2.0' and 'V2.1' are supported. Version 'V2.2' was found.")));
+                "RangeError (index): Index out of range: no indices are valid: 0")));
   });
 }
