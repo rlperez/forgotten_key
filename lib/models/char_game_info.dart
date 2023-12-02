348fa7dc759d1d1a539d6d92d478c9e5ba4b39b1
Rigoberto L. Perez <rigperez@proton.me>
2023-12-02 11:50:01 -0500

    feat: 🎸 add read to read version from the file

--- lib/models/char_game_info.dart
+++ lib/models/char_game_info.dart
@@ -1,11 +1,16 @@
 import 'package:forgotten_key/util/char_game_info_reader.dart';
+import 'package:loggy/loggy.dart';
 
-class CharGameInfo {
+class CharGameInfo with UiLoggy {
   final String name;
   final String path;
   final String header;
-
-  CharGameInfo({required this.name, required this.path, required this.header});
+  final String gameVersion;
+  CharGameInfo(
+      {required this.name,
+      required this.path,
+      required this.header,
+      required this.gameVersion});
 
   static Future<CharGameInfo> read(String path) async {
     final info = CharGameInfoReader(path).read();
--- lib/util/char_game_info_reader.dart
+++ lib/util/char_game_info_reader.dart
@@ -13,11 +13,13 @@ class CharGameInfoReader with UiLoggy {
     try {
       file = await File(path).open();
       final header = _readHeader(file);
+      final gameVersion = _readVersion(file);
 
       return CharGameInfo(
         name: 'Unknown',
         path: path,
         header: header,
+        gameVersion: gameVersion,
       );
     } catch (e) {
       loggy.error(e);
@@ -32,12 +34,26 @@ class CharGameInfoReader with UiLoggy {
   String _readHeader(RandomAccessFile file) {
     final header = file.readSync(4);
     String headerString = String.fromCharCodes(header);
+    logDebug("header: $headerString");
 
     if (headerString != 'GAME') {
       logError('Invalid header');
       throw Exception('Invalid header of $headerString. It should be GAME.');
     }
-
     return headerString;
+  }
+
+  String _readVersion(RandomAccessFile file) {
+    final version = file.readSync(4);
+    String versionString = String.fromCharCodes(version);
+    logDebug("gameVersion: $versionString");
+
+    if (versionString != 'V2.0' && versionString != 'V2.1') {
+      logError('Invalid version: $versionString');
+      throw Exception(
+          "Invalid version of '$versionString'. Only 'V2.0' and 'V2.1' are supported. Version '$versionString' was found.");
+    }
+
+    return versionString;
   }
 }
