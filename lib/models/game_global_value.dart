311ab16e60beb452042d114e02e7a342cfe50764
Rigoberto L. Perez <rigperez@proton.me>
2023-12-02 21:10:34 -0500

    refactor: 💡 rename file to be more consistent

--- lib/models/game_global_value.dart
+++ lib/models/game_global_value.dart
@@ -1,16 +1,21 @@
 import 'dart:typed_data';
 
 // 84 bytes total
-class InfGameGlobal {
+class GameGlobalValue {
   final String name;
   final Uint8List unknown0;
   final int value;
   final Uint8List unknown2;
 
-  InfGameGlobal({
+  GameGlobalValue({
     required this.name,
     required this.unknown0,
     required this.value,
     required this.unknown2,
   });
+
+  @override
+  String toString() {
+    return 'GameGlobalValue {name: $name, unknown0: $unknown0, value: $value, unknown2: $unknown2}';
+  }
 }
