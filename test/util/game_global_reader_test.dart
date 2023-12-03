c694bfa9180c0c8d63c4b9831d618b6407a61ed4
Rigoberto L. Perez <rigperez@proton.me>
2023-12-02 21:57:34 -0500

    test: 💍 add a first test on global bars

--- test/util/game_global_reader_test.dart
+++ test/util/game_global_reader_test.dart
@@ -13,9 +13,115 @@ void main() {
 
     expect(values, isNotNull);
     expect(values.length, 161);
-    expect(values.first.name, 'AR0700');
-    expect(values.first.value, 0);
+    expect(values.first.name,
+        'METVAI\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00');
+    expect(values.first.value, 1);
     expect(values.first.unknown0, [0, 0, 0, 0, 0, 0, 0, 0]);
-    expect(values.first.unknown2, [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
+    expect(values.first.unknown1, [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]);
+    expect(values.last.value, 0);
+    expect(values.last.unknown0, [0, 0, 0, 0, 0, 0, 0, 0]);
+    expect(values.last.unknown1, [
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0,
+      0
+    ]);
+    expect(values.last.name,
+        'BD_STORY_MODE\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00');
+  });
+
+  test('should throw an exception if the file is invalid', () async {
+    final file = await File('test_file.gam').create();
+    final randomAccessFile = await file.open(mode: FileMode.write);
+    const version = 'V2.1';
+    await randomAccessFile.writeString('GAME');
+    await randomAccessFile.writeString(version);
+    await randomAccessFile.flush();
+    await randomAccessFile.setPosition(0);
+
+    final reader = GameGlobalValueReader(file: randomAccessFile);
+    expect(
+        reader.read(0, 1),
+        throwsA(predicate((e) =>
+            e is RangeError &&
+            e.toString() ==
+                "RangeError (index): Index out of range: no indices are valid: 0")));
   });
 }
