cf67b0f579dc34b3212f142b011459520e92b7af
Rigoberto L. Perez <rigperez@proton.me>
2023-12-02 21:56:58 -0500

    fix: 🐛 add a toString function

--- lib/models/game_global_value.dart
+++ lib/models/game_global_value.dart
@@ -5,17 +5,17 @@ class GameGlobalValue {
   final String name;
   final Uint8List unknown0;
   final int value;
-  final Uint8List unknown2;
+  final Uint8List unknown1;
 
   GameGlobalValue({
     required this.name,
     required this.unknown0,
     required this.value,
-    required this.unknown2,
+    required this.unknown1,
   });
 
   @override
   String toString() {
-    return 'GameGlobalValue {name: $name, unknown0: $unknown0, value: $value, unknown2: $unknown2}';
+    return 'GameGlobalValue {name: $name, unknown0: $unknown0, value: $value, unknown1: $unknown1}';
   }
 }
